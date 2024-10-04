4fe72194b0e5e167ce77778734ae10be6f1a2fc0
dang11-ly <layaoamandayvelle@gmail.com>
2024-10-04 21:57:10 +0800

    Add files via upload

---
+++ evenNumber.js
@@ -0,0 +1,11 @@
+// Print numbers from 1 to 10 using a for loop
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+// Print only even numbers between 1 and 10
+for (let i = 1; i <= 10; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
